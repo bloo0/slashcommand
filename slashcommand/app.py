044452ccc9a4f361ef90d16994172a0c33671694
Phil Manansala <phil@phil.com>
2022-10-11 00:05:53 +0800

    remove test

--- slashcommand/app.py
+++ slashcommand/app.py
@@ -2,8 +2,6 @@
 from slashcommand.secgroup import ip_add, ip_clr, status
 
 def run():
-    print(ip_clr.cleanIngress('sg-0d3edb7db7b5e36db','eu-north-1'))
-    #print(status.viewIngress('sg-0d45f6f4686fc0055','eu-north-1'))
-    #print(ip_add.vpnAdd('158.62.32.38/32','sg-0d45f6f4686fc0055','eu-north-1'))
+    pass
 
 
